6f6c058d19ba6b714adb96dd075ec6b18d443c54
TAEJUN-H <44741814+TAEJUN-H@users.noreply.github.com>
2019-09-13 04:37:26 +0900

    mypage(profile, saleslist, purchaselist) 작성

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.get("/product_details/:_id", async function(req, res, next) {
       req: req,
       product
     };
-  
+
     res.render("product_details", { req });
   } catch (e) {
     res.render("product_details", { req });
@@ -52,6 +52,7 @@ router.get("/contact", function(req, res, next) {
   };
   res.render("contact", { req });
 });
+
 router.get("/cart", function(req, res, next) {
   res.locals = {
     title: "장바구니",
@@ -59,5 +60,68 @@ router.get("/cart", function(req, res, next) {
   };
   res.render("cart", { req });
 });
+
+/*
+*  /pagetest 엔드포인트 요청(request, req)이 들어오면
+*  view폴더에 있는 pagetest.ejs 파일을 렌더링해서 출력한다.
+*/
+router.get("/pagetest", function(req, res, next) { // URL Endpoint (http://[host]:[port]/endpoint)
+  res.locals = {
+    title: "페이지테스트", // 페이지 제목
+    req: req // http req 변수. 그냥 넘겨주면 됨.
+  };
+
+  res.render("pagetest.ejs", { req }); // views폴더에 있는 xxx.ejs 파일을 렌더링. .ejs는 생략가능
+});
+
+router.get("/mypage", function(req, res, next) {
+  res.locals = {
+    title: "마이페이지",
+    req: req
+  };
+  res.render("mypage", { req });
+});
+
+router.get("/saleslist", function(req, res, next) {
+  res.locals = {
+    title: "판매 목록",
+    req: req
+  };
+  res.render("saleslist", { req });
+});
+
+router.get("/sales_details", function(req, res, next) {
+  res.locals = {
+    title: "판매 상세",
+    req: req
+  };
+  res.render("sales_details", { req });
+});
+
+router.get("/purchaselist", function(req, res, next) {
+  res.locals = {
+    title: "구매 목록",
+    req: req
+  };
+  res.render("purchaselist", { req });
+});
+
+router.get("/order_details", function(req, res, next) {
+  res.locals = {
+    title: "주문 상세",
+    req: req
+  };
+  res.render("order_details", { req });
+});
+
+// This is Temp!! Delete later
+router.get("/product_details_temp", function(req, res, next) {
+  res.locals = {
+    title: "(임시)상품 상세",
+    req: req
+  };
+  res.render("product_details_temp", { req });
+});
+
 
 module.exports = router;
